d83732dd1dbeac31ddd02438ebb71e8ac75b215c
marchennn <marchendynugraha.2022@student.uny.ac.id>
2025-08-24 13:18:51 +0700

    tolong bisa

--- scripts/seed-bundle.ts
+++ scripts/seed-bundle.ts
@@ -5,6 +5,7 @@ import { eq } from 'drizzle-orm';
 async function main() {
   const seedCategories = [
     {
+      id: 'bisnis-entrepreneurship',
       name: 'Bisnis & Entrepreneurship',
       slug: 'bisnis-entrepreneurship',
       description: 'Ebook bisnis dan entrepreneurship, 100+ ebook.',
@@ -15,6 +16,7 @@ async function main() {
       driveLink: 'https://drive.google.com/drive/folders/1o_O_lwAVqiyZxJEBnE-9aws5lScSMmH-?usp=drive_link',
     },
     {
+      id: 'digital-marketing',
       name: 'Digital Marketing',
       slug: 'digital-marketing',
       description: 'Ebook digital marketing, 100+ ebook.',
@@ -25,6 +27,7 @@ async function main() {
       driveLink: 'https://drive.google.com/drive/folders/1SU3EbZ1TCsZ4TL1RTb3rcd_Bc4eeq5vj?usp=drive_link',
     },
     {
+      id: 'kesehatan-lifestyle',
       name: 'Kesehatan & Lifestyle',
       slug: 'kesehatan-lifestyle',
       description: 'Ebook kesehatan dan lifestyle, 100+ ebook.',
@@ -35,6 +38,7 @@ async function main() {
       driveLink: 'https://drive.google.com/drive/folders/13kHX_d1BjAtS1mxObgia91-zyhVkeMu7?usp=drive_link',
     },
     {
+      id: 'keuangan-investasi',
       name: 'Keuangan & Investasi',
       slug: 'keuangan-investasi',
       description: 'Ebook keuangan dan investasi, 100+ ebook.',
@@ -45,6 +49,7 @@ async function main() {
       driveLink: 'https://drive.google.com/drive/folders/1Qnlow_bAjXCwnpHOy2b1t0c809ETPRZI?usp=drive_link',
     },
     {
+      id: 'kreatif-desain',
       name: 'Kreatif & Desain',
       slug: 'kreatif-desain',
       description: 'Ebook kreatif dan desain, 100+ ebook.',
@@ -55,6 +60,7 @@ async function main() {
       driveLink: 'https://drive.google.com/drive/folders/1ZK5V5sj33pQecTkgxuKHBqgCbLDXaYF1?usp=drive_link',
     },
     {
+      id: 'pendidikan-pengembangan-diri',
       name: 'Pendidikan & Pengembangan Diri',
       slug: 'pendidikan-pengembangan-diri',
       description: 'Ebook pendidikan dan pengembangan diri, 100+ ebook.',
@@ -65,6 +71,7 @@ async function main() {
       driveLink: 'https://drive.google.com/drive/folders/1hVzrqSDMcQhGVO_f_v9GU7634aiXShRi?usp=drive_link',
     },
     {
+      id: 'teknologi-programming',
       name: 'Teknologi & Programming',
       slug: 'teknologi-programming',
       description: 'Ebook teknologi dan programming, 100+ ebook.',
@@ -75,6 +82,7 @@ async function main() {
       driveLink: 'https://drive.google.com/drive/folders/1ul9fwSLw_FPHRlj2y4BjXI4-XGWDOyU8?usp=drive_link',
     },
     {
+      id: 'bundle-special-7-kategori',
       name: 'Bundle Special 7 Kategori',
       slug: 'bundle-special-7-kategori',
       description: 'Akses semua ebook dari 7 kategori sekaligus, 700+ ebook.',
