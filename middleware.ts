4f3eb868df86fa12a790c7a916888f23ec8e0508
marchennn <marchendynugraha.2022@student.uny.ac.id>
2025-08-24 20:08:00 +0700

    percobaan lagi dan lagi

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const response = NextResponse.next();
+  response.headers.set(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://app.sandbox.midtrans.com https://api.sandbox.midtrans.com https://snap-assets.al-pc-id.b.cdn.gtflabs.io https://pay.google.com https://js-agent.newrelic.com http://bam.nr-data.net; style-src 'self' 'unsafe-inline'; img-src * blob: data:; connect-src *; frame-src https://app.sandbox.midtrans.com https://app.midtrans.com;"
+  );
+  return response;
+}
+
+export const config = {
+  matcher: "/:path*",
+};
