d733168c729ddbe3f6a26b9bfd319cdf80ac020f
marchennn <marchendynugraha.2022@student.uny.ac.id>
2025-08-24 19:24:55 +0700

    percobaan ketiga kali

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
+
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' https://app.sandbox.midtrans.com https://api.sandbox.midtrans.com https://snap-assets.al-pc-id.b.cdn.gtflabs.io https://pay.google.com https://js-agent.newrelic.com http://bam.nr-data.net;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://app.sandbox.midtrans.com https://api.sandbox.midtrans.com https://snap-assets.al-pc-id.b.cdn.gtflabs.io https://pay.google.com https://js-agent.newrelic.com http://bam.nr-data.net;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   connect-src *;
   frame-src https://app.sandbox.midtrans.com https://app.midtrans.com;
 `;
 
-module.exports = {
-  // ...existing config...
+const nextConfig = {
   async headers() {
     return [
       {
@@ -22,22 +22,7 @@ module.exports = {
       },
     ];
   },
+  // ...tambahkan config lain jika perlu...
 };
-import type { NextConfig } from "next";
 
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://snap-assets.al-pc-id-b.cdn.gftlabs.io https://api.sandbox.midtrans.com https://app.sandbox.midtrans.com;"
-          }
-        ]
-      }
-    ];
-  },
-};
 export default nextConfig;
