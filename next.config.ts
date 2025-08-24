cc73a8edfd0c18cfffe41caf9573a8b873fc2d41
marchennn <marchendynugraha.2022@student.uny.ac.id>
2025-08-24 16:45:39 +0700

    semuanya sudah benar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' https://snap-assets.al-pc-id-b.cdn.gftlabs.io https://api.sandbox.midtrans.com https://app.sandbox.midtrans.com 'unsafe-eval';"
+          }
+        ]
+      }
+    ];
+  },
 };
 
 export default nextConfig;
